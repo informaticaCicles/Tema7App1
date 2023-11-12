05134e9af9dcebc8f82baf296911ce3bd8dc503f
Anna <informaticaCicles@gmail.com>
2023-11-12 23:16:17 +0100

    1st example

---
+++ app/src/main/java/com/example/tema7app1/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.tema7app1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import com.example.tema7app1.databinding.ActivityMainBinding
+import com.example.tema7app1.databinding.ToolbarBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolBar: androidx.appcompat.widget.Toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.appbar)
+        setSupportActionBar(toolBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_settings ->{
+                showToast("Opción settings seleccionada")
+                true
+            }
+            R.id.action_buscar ->{
+                showToast("Opción search seleccionada")
+                true
+            }
+            R.id.action_nuevo ->{
+                showToast("Opción add seleccionada")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+    }
+}
